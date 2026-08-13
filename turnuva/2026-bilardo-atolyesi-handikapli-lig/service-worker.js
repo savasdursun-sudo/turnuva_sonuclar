ae0a870e7155e5f5bb9678f8d5cc4da1fefc362e
Savaş Dursun <savasdursun@gmail.com>
2026-08-13 19:35:11 +0300

    Yayin guncelle: 2026 Bilardo Atölyesi Handikaplı Lig 20260813193413936289

--- turnuva/2026-bilardo-atolyesi-handikapli-lig/service-worker.js
+++ turnuva/2026-bilardo-atolyesi-handikapli-lig/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "20260813111539433985";
+const CACHE_VERSION = "20260813193413936289";
 const STATIC_CACHE = `turnuva-sonuclari-static-${CACHE_VERSION}`;
 const DATA_CACHE = `turnuva-sonuclari-data-${CACHE_VERSION}`;
 const STATIC_FILES = [
